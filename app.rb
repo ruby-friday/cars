34091e918cd9f8e6970ebd59aba6a9b9c64c0c4c
Andrew Mukiza <andrewmukiza@gmail.com>
2014-11-28 14:02:14 +0300

    add form and save to database

--- app.rb
+++ app.rb
@@ -2,16 +2,20 @@ require_relative './car.rb'
 require 'sinatra'
 
 get '/' do
-	Car.destroy
-  Car.create(color: 'John', brand: 'toyota', year: 2003)
-  Car.create(color: 'John', brand: 'toyota', year: 2003)
-  Car.create(color: 'John', brand: 'toyota', year: 2003)
-  Car.create(color: 'John', brand: 'BMW', year: 2012)
-  Car.create(color: 'John', brand: 'benz', year: 2003)
-  Car.create(color: 'John', brand: 'toyota', year: 2003)
-
-  @cars = Car.all
-  @toyotas = Car.all(brand: 'toyota')
+	@cars = Car.all
   erb :cars  #renders views/cars.erb
 end
 
+
+get '/new' do
+	erb :new
+end
+
+post '/create' do
+	@car = Car.new(color: params['color'], year: params['year'], brand: params['brand'])
+	if @car.save
+		redirect '/'
+	else
+		erb :new
+	end
+end
